fd8e5db58c4a2598b5fae5f9dca27b41eca6b7e0
Leon <leonh@Leon-Windows.staff.7d>
2016-02-23 11:54:19 +0000

    Replace XUnit with NUnit

--- Composition/CS/Composition/Algorithm.Tests/Composition/AggregationTests.cs
+++ Composition/CS/Composition/Algorithm.Tests/Composition/AggregationTests.cs
@@ -1,67 +1,69 @@
 ﻿using Algorithm.Composition;
-using Xunit;
+using NUnit.Framework;
+
 
 namespace Algorithm.Tests.Composition
 {
+	[TestFixture]
     public class AggregationTests
     {
-        [Fact]
+        [Test]
         public void SummingAggregation_Produces_Sum()
         {
             var aggregator = new PointsAggregator(_measurements, new EmptyFilter(), new SummingStrategy());
 
             var result = aggregator.Aggregate();
 
-            Assert.Equal(107, result.X);
-            Assert.Equal(30, result.Y);
+            Assert.That(result.X, Is.EqualTo(107));
+            Assert.That(result.Y, Is.EqualTo(30));
         }
 
-        [Fact]
+        [Test]
         public void AveragingAggregagtor_Produces_Average()
         {
             var aggregator = new PointsAggregator(_measurements, new EmptyFilter(), new AveragingStrategy());
 
             var result = aggregator.Aggregate();
 
-            Assert.Equal(35, result.X);
-            Assert.Equal(10, result.Y);
+			Assert.That(result.X, Is.EqualTo(35));
+			Assert.That(result.Y, Is.EqualTo(10));
         }
 
-        [Fact]
+        [Test]
         public void LowPassAveragingAggregator_Applys_Filter()
         {
             var aggregator = new PointsAggregator(_measurements, new LowPassFilter(), new AveragingStrategy());
 
             var result = aggregator.Aggregate();
 
-            Assert.Equal(3, result.X);
-            Assert.Equal(12, result.Y);            
+			Assert.That(result.X, Is.EqualTo(3));
+			Assert.That(result.Y, Is.EqualTo(12));            
         }
 
         // Uncomment this test and make it pass reusing as much code as 
         // possible from other classes that are available in the Composition folder
-        //[Fact]
+        //[Test]
         //public void HighPassSummingAggregator_Applys_Filter()
         //{                
         //    var aggregator = new PointsAggregator(_measurements, new HighPassFilter(), new SummingStrategy());
 
         //    var result = aggregator.Aggregate();
 
-        //    Assert.Equal(105, result.X);
-        //    Assert.Equal(15, result.Y);
+		//    Assert.That(result.X, Is.EqualTo(105));
+		//    Assert.That(result.Y, Is.EqualTo(15));
         //}
 
         /// Uncomment this test and make it pass by building a new class to 
         /// hide the "composition" of strategies and filters ...
-        //[Fact]
+        //[Test]
         //public void CustomHighPassSummingAggregator_Applys_Filter()
         //{                
         //    var aggregator = new HighPassSummingAggregator(_measurements);
 
         //    var result = aggregator.Aggregate();
 
-        //    Assert.Equal(105, result.X);
-        //    Assert.Equal(15, result.Y);
+        //    Assert.That(result.X,Is.EqualTo(105));
+		//    Assert.That(result.Y,Is.EqualTo(15));
         //}
 
         Measurement[] _measurements = new[]
--- Composition/CS/Composition/Algorithm.Tests/Inheritance/AggregationTests.cs
+++ Composition/CS/Composition/Algorithm.Tests/Inheritance/AggregationTests.cs
@@ -1,54 +1,55 @@
 ﻿using Algorithm.Inheritance;
-using Xunit;
+using NUnit.Framework;
 
 namespace Algorithm.Tests.Inheritance
 {
+	[TestFixture]
     public class AggregationTests
     {
-        [Fact]
+        [Test]
         public void SummingAggregation_Produces_Sum()
         {
             var aggregator = new SummingAggregator(_measurements);
 
             var result = aggregator.Aggregate();
 
-            Assert.Equal(107, result.X);
-            Assert.Equal(30, result.Y);
+            Assert.That(result.X, Is.EqualTo(107));
+            Assert.That(result.Y, Is.EqualTo(30));
         }
 
-        [Fact]
+        [Test]
         public void AveragingAggregagtor_Produces_Average()
         {
             var aggregator = new AveragingAggregator(_measurements);
 
             var result = aggregator.Aggregate();
 
-            Assert.Equal(35, result.X);
-            Assert.Equal(10, result.Y);
+            Assert.That(result.X, Is.EqualTo(35));
+            Assert.That(result.Y, Is.EqualTo(10));
         }
 
-        [Fact]
+        [Test]
         public void LowPassAveragingAggregator_Applys_Filter()
         {
             var aggregator = new LowPassAveragingAggregator(_measurements);
 
             var result = aggregator.Aggregate();
 
-            Assert.Equal(3, result.X);
-            Assert.Equal(12, result.Y);            
+            Assert.That(result.X, Is.EqualTo(3));
+            Assert.That(result.Y, Is.EqualTo(12));            
         }
 
         /// Uncomment this test and make it pass reusing as much code as 
         /// possible from other classes that are availalbe in the Inheritance folder
-        //[Fact]
+        //[Test]
         //public void HighPassSummingAggregator_Applys_Filter()
         //{                
         //    var aggregator = new HighPassSummingAggregator(_measurements);
 
         //    var result = aggregator.Aggregate();
 
-        //    Assert.Equal(105, result.X);
-        //    Assert.Equal(15, result.Y);
+		//    Assert.That(result.X,Is.EqualTo(105));
+		//    Assert.That(result.Y,Is.EqualTo(15));
         //}
 
         Measurement[] _measurements = new[]
